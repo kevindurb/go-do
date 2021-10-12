b8f966f7c00073db22da8ed006b136dde8499c51
Kevin Durbin <kevindurb@gmail.com>
2021-10-12 07:47:56 -0600

    move parsing to uint to util fn

--- handlers/UpdateTodoItem.go
+++ handlers/UpdateTodoItem.go
@@ -5,18 +5,15 @@ import (
 	"kevindurb/go-do/models"
 	"kevindurb/go-do/utils"
 	"net/http"
-	"strconv"
-
-	"github.com/gorilla/mux"
 )
 
 func UpdateTodoItem(response http.ResponseWriter, request *http.Request) {
 	db := utils.GetConnection()
-	id, _ := strconv.ParseUint(mux.Vars(request)["id"], 10, 64)
+	id := utils.ParseUIntParam(request, "id")
 
 	var todoItem models.TodoItem
 	utils.ParseBody(request, &todoItem)
-	db.Model(&models.TodoItem{ID: uint(id)}).Updates(&todoItem)
-	db.First(&todoItem, uint(id))
+	db.Model(&models.TodoItem{ID: id}).Updates(&todoItem)
+	db.First(&todoItem, id)
 	json.NewEncoder(response).Encode(todoItem)
 }
--- utils/Rest.go
+++ utils/Rest.go
@@ -4,10 +4,19 @@ import (
 	"encoding/json"
 	"io/ioutil"
 	"net/http"
+	"strconv"
+
+	"github.com/gorilla/mux"
 )
 
 func ParseBody(request *http.Request, value interface{}) {
 	defer request.Body.Close()
   body, _ := ioutil.ReadAll(request.Body)
 	json.Unmarshal(body, value)
+}
+
+func ParseUIntParam(request *http.Request, key string) uint {
+	vars := mux.Vars(request)
+	intVal, _ := strconv.ParseUint(vars[key], 10, 64)
+	return uint(intVal)
 }
